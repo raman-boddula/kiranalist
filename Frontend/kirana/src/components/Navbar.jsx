a23a5e1a58491f02a8091d3e16635b7599996e2b
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-03-03 17:35:49 +0530

    added Navbar in react

--- Frontend/kirana/src/components/Navbar.jsx
+++ Frontend/kirana/src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 // https://www.kiranalist.com/media/image/0/default-image.png?size=250
 import React from 'react';
-import image from "../../public/kiranaList.jpg";
 export const Navbar = () => {
     const [user, setUser] = React.useState("");
     return (
         <>
             <div className="navbar">
                 <div>
-                    <img src={image} alt="logo" />
+                    <img src="https://raw.githubusercontent.com/raman-boddula/kiranalist/main/Frontend/kirana/public/kiranaList.jpg" alt="logo" />
                 </div>
                 <div>
                     <p>Welcome {user}</p>

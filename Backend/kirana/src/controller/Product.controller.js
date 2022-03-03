6dc2a0490ee99de65917cd49eccb0037e89e8af5
Raman Boddula <raman_rc001@yahoo.com>
2022-03-03 19:09:46 +0530

    added pagination

--- Backend/kirana/src/controller/Product.controller.js
+++ Backend/kirana/src/controller/Product.controller.js
@@ -15,8 +15,14 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const product = await Product.find().lean().exec();
-    res.status(200).json({ product });
+    const page = +req.query.Page || 1;
+    const size = +req.query.size || 6;
+    const offset = (page - 1) * size;
+    const product = await Product.find().skip(offset).limit(size).lean().exec();
+    const total_pages = Math.ceil(
+      (await Product.find().countDocuments()) / size
+    );
+    res.status(200).json({ products: product, total_pages: total_pages });
   } catch (e) {
     res.status(404).json({ message: e.message, status: "Failed" });
   }

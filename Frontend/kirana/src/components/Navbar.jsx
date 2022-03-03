5cc2e7bc98f5d9a81a657912a9f361660d5cabfe
Raman Boddula <raman_rc001@yahoo.com>
2022-03-04 00:49:24 +0530

    fixed login details

--- Frontend/kirana/src/components/Navbar.jsx
+++ Frontend/kirana/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
 // https://www.kiranalist.com/media/image/0/default-image.png?size=250
 import React from 'react';
 import { Dropdown, Menu } from 'antd';
+import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
     const userName = JSON.parse(localStorage.getItem('isAuth'))?.user?.Name;
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem('isAuth');
+        navigate('/login')
+    }
     const menu = (
         <Menu>
-            <Menu.Item key="1">Logout</Menu.Item>
+            <Menu.Item key="1" onClick={handleLogout}>Logout</Menu.Item>
             <Menu.Item key="2">Show Previous</Menu.Item>            
         </Menu>
       );
@@ -16,10 +22,11 @@ export const Navbar = () => {
                 <div>
                     <img src="https://raw.githubusercontent.com/raman-boddula/kiranaList/main/Frontend/kirana/public/kiranaList.jpg" alt="logo" />
                 </div>
-                <div>
-                    <Dropdown.Button overlay={menu} placement="bottomRight" style={{marginTop:"10px"}}>Welcome {userName}</Dropdown.Button>
-                    <img src="https://cdn-icons-png.flaticon.com/512/650/650661.png" alt="Notepad"/>
-                </div>
+                {
+                    userName ?<div>
+                    <Dropdown.Button overlay={menu} placement="bottomRight" style={{ marginTop: "10px" }}>Welcome {userName}</Dropdown.Button>
+                    <img src="https://cdn-icons-png.flaticon.com/512/650/650661.png" alt="Notepad" />
+                &ensp;</div>:null}
             </div>
         </>
     )

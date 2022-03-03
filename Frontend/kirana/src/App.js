b27aa0c468cb0de1959f9ede517ae382433f5a5e
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-03-03 19:12:48 +0530

    added HomePage in react

--- Frontend/kirana/src/App.js
+++ Frontend/kirana/src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-<<<<<<< HEAD
 import { HomePage } from "./components/HomePage";
-
-=======
 import { Routes, Route } from "react-router-dom";
 import { Register } from "./components/Register";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { Login } from "./components/Login";
->>>>>>> 6dc2a0490ee99de65917cd49eccb0037e89e8af5
 function App() {
   return (
     <div className="App">
       <Navbar />
-<<<<<<< HEAD
       <HomePage />
-=======
       <Routes>
         <Route path="/register" element={<Register />}></Route>
         <Route
@@ -25,7 +19,6 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route></Route>
       </Routes>
->>>>>>> 6dc2a0490ee99de65917cd49eccb0037e89e8af5
     </div>
   );
 }

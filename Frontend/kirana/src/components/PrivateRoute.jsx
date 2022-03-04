23edb5ffd2bb51ad87e000d9b8ef530e5776f28b
Raman Boddula <raman_rc001@yahoo.com>
2022-03-04 13:54:15 +0530

    updated localSto to SessionSto

--- Frontend/kirana/src/components/PrivateRoute.jsx
+++ Frontend/kirana/src/components/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import {Navigate} from "react-router-dom"
 export const PrivateRoute = ({ children }) => {
-    const isAuth = JSON.parse(localStorage.getItem("isAuth"))?.token;
+    const isAuth = JSON.parse(sessionStorage.getItem("isAuth"))?.token;
     if (!isAuth) { 
         return <Navigate to="/login"></Navigate>
     }

054ea9aa1557074e090cdbf82b3c0a2ead7773b3
vasukomuravelli <vasukomuravelli96@gmail.com>
2022-03-05 11:56:06 +0530

    buttonchange

--- Frontend/kirana/src/components/Product.jsx
+++ Frontend/kirana/src/components/Product.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Button } from 'antd';
+import { ListContext } from "../contexts/ListContext"
+import { Link } from "react-router-dom";
 
 export const Product = ({ product,handleShowDiv }) => {
     const [showbutton, setShowbutton] = React.useState(false);
+    const { list } = React.useContext(ListContext);
     return (
         <div onMouseEnter={() => setShowbutton(true)} onMouseLeave={() => setShowbutton(false)}>
             <div className="productDetails">
                 <img src={product?.Image[0]} alt={product.Name} /><br/><br/>
                 <h4>{product.Name}</h4><br/>
             </div>
-            {showbutton ? <Button type="primary" shape="round" size="large" onClick={()=>handleShowDiv(product._id)}>Add Quantity</Button> : null}
+            {showbutton && list.map((e)=>(e.Name===product.Name && <Link to="/list"><Button type="primary" shape="round" size="large">Go To List</Button></Link>)) ? <Button type="primary" shape="round" size="large" onClick={()=>handleShowDiv(product._id)}>Add Quantity</Button> : null}
         </div>
     )
 }
